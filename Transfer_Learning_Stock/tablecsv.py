d55696b2ac5ad1a2df6b38aed57fd5e14818c070
fihrizilhamr <116176265+fihrizilhamr@users.noreply.github.com>
2023-10-08 17:17:26 +0700

    adding stock market prediction

--- Transfer_Learning_Stock/tablecsv.py
+++ Transfer_Learning_Stock/tablecsv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Replace this with the path to your text file
-text_file_path = 'D:\\RSBP\\Transfer_Learning_Stock\\usdeur_datasets.txt'
+text_file_path = r'D:\Kuliah\Semester 5\RSBP\Transfer-Learning-Stock\Transfer_Learning_Stock\usdeur_datasets.txt'
 
 # Read the text data and split it into rows and columns
 with open(text_file_path, 'r') as text_file:
@@ -12,7 +12,7 @@ with open(text_file_path, 'r') as text_file:
 df = pd.DataFrame(data)
 
 # Replace this with the desired Excel file path
-csv_file_path = 'D:\\RSBP\\Transfer_Learning_Stock\\usdeur_fix.csv'
+csv_file_path = r'D:\Kuliah\Semester 5\RSBP\Transfer-Learning-Stock\Transfer_Learning_Stock\usdeur_fix.csv'
 
 # Write the DataFrame to an Excel file
 df.to_csv(csv_file_path, index=False, header=False)

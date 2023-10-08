efe705f4932def681662946a6017fdc3c45f66e3
fihrizilhamr <116176265+fihrizilhamr@users.noreply.github.com>
2023-10-08 17:53:44 +0700

    small fix

--- Stock_Market_Prediction/Tesla stock price prediction.py
+++ Stock_Market_Prediction/Tesla stock price prediction.py
@@ -7,7 +7,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
+# get_ipython().run_line_magic('matplotlib', 'inline')
 
 import chart_studio.plotly as py
 import plotly.graph_objs as go
